3e250a3e1a9e1b89d0a7d2000db8f29f618daa44
DevShah-DevsTreeIT <dev.s@devstree.in>
2025-09-03 10:13:26 +0530

    Initial commit for my project

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.db import models
+import uuid
+
+
+# User Table
+class User(models.Model):
+    # id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField(unique=True)  #here when we use unique=True it will not allow duplicate emails 
+    user_password = models.CharField(max_length=255)  # Later you can hash this
+    auth_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True) #This field is designed to store Universally Unique Identifiers (UUIDs) and possesses specific characteristics
+    # auth_token = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user_name    
