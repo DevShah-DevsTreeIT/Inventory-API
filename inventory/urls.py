21c43c5e81af3f47e663f506eeaa9912a8f4c337
DevShah-DevsTreeIT <dev.s@devstree.in>
2025-09-04 13:21:43 +0530

    updated inventory with csrf urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,4 @@
 # Admin
-
-
 """
 URL configuration for inventory project.
 
@@ -19,8 +17,44 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
+from django.http import JsonResponse
+
+def csrf(request):
+    return JsonResponse({"csrfToken": get_token(request)})
 
 urlpatterns = [
+    # path("csrf/", ensure_csrf_cookie(get_csrf_token)),
     path("admin/", admin.site.urls),
+    
+    # users app (register, login, me)
     path("users/", include("users.urls")),  # <--- important
+    
+    # products app (categories, products)
+    path("products/", include("products.urls")),  # <--- important
+
+    # global csrf
+    path("csrf/", csrf, name="csrf"),
 ]
+
+
+# from django.contrib import admin
+# from django.urls import path
+# from users import views as user_views
+# from products import views as product_views
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+
+#     # ---- User Auth ----
+#     path("users/register/", user_views.register, name="register"),
+#     path("users/login/", user_views.login, name="login"),
+#     path("users/me/", user_views.me, name="me"),
+
+#     # ---- Category ----
+#     path("categories/", product_views.categories, name="categories"),
+
+#     # ---- Product ----
+#     path("products/", product_views.products, name="products"),
+# ]
